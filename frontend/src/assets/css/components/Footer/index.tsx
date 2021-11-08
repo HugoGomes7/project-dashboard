8ce1e9478502e4c213aac6eb49f3e3df1054fdbb
Hugo Gomes <hugorgomes10@gmail.com>
2021-11-07 23:51:30 -0300

    Basic static components

---
+++ frontend/src/assets/css/components/Footer/index.tsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <div>
+      <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/HugoGomes7" target="_blank" rel="noreferrer">Hugo Gomes</a></p>
+          <p className="text-light"><small><strong>Project Spring Boot React</strong><br />
+            Siga-me: <a href="https://www.instagram.com/hugo_gomess7/" target="_blank" rel="noreferrer">@hugo_gomess7.ig</a></small></p>
+        </div>
+      </footer>
+
+    </div>
+  );
+}
+
+export default Footer;
