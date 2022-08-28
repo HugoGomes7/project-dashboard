b256f55a625e4d1974c32f5702879e306d528830
Hugo Gomes <hugorgomes10@gmail.com>
2022-08-28 01:54:13 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,3 +1,26 @@
+import { Seller } from "./seller";
+
+export type Sale = {
+  id: number;
+  visited: number;
+  deals: number;
+  amount: number;
+  date: string;
+  seller: Seller;
+}
+
+export type SalePage = {
+  content?: Sale[]; // opcional
+  last: boolean;
+  totalPages: number;
+  totalElements: number;
+  size?: number; // opcional
+  number: number;
+  first: boolean;
+  numberOfElements?: number; // opcional
+  empty?: boolean; // opcional
+}
+
 export type SaleSum = {
   sellerName: string;
   sum: number;
