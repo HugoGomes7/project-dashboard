2f14dd818bf2b7e2c66835c049aee82744f21b61
Hugo Gomes <hugorgomes10@gmail.com>
2022-09-12 13:23:54 -0300

    env var and updates

--- frontend/src/assets/css/components/Footer/index.tsx
+++ frontend/src/assets/css/components/Footer/index.tsx
@@ -6,6 +6,8 @@ function Footer() {
           <p className="text-light">App desenvolvido por <a href="https://github.com/HugoGomes7" target="_blank" rel="noreferrer">Hugo Gomes</a></p>
           <p className="text-light"><small><strong>Project Spring Boot React</strong><br />
             Siga-me: <a href="https://www.instagram.com/_hugodlucca/" target="_blank" rel="noreferrer">@_hugodlucca.ig</a></small></p>
+          <p className="text-light"><small><strong>Documentação completa do projeto no Notion </strong><br />
+            Disponível em: <a href="https://abrir.link/gSh33" target="_blank" rel="noreferrer">Notion Project Spring Boot React</a></small></p>
         </div>
       </footer>
 
--- frontend/src/utils/requests.ts
+++ frontend/src/utils/requests.ts
@@ -1,1 +1,10 @@
-export const BASE_URL = 'http://localhost:8080';
+export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+/* 
+O ?? (operador de coalescência nula) significa que:
+se a variável de ambiente "process.env.REACT_APP_BACKEND_URL" não estiver definida 
+no ambiente onde a aplicação estiver rodando, (por exemplo: na minha máquina local ela não esta definida)
+dessa forma, vai pegar o valor que estiver a direita.
+Isso serve para rodar tanto localmente pegando o backend do localhost, como no Netlify rodando na nuvem
+pegando o valor da variável de ambiente que foi definida
+ */
